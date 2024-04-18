5def556f0c9919b847e2bedf6d38eda8c4a58069
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2024-04-18 19:12:21 +0200

    v1.0

--- js/main.js
+++ js/main.js
@@ -13,12 +13,22 @@ window.addEventListener('resize', adjustMasonry);
 /** FUNCTIONS **/
 /*****************************************************************************/
 
+/**
+ * Checks if an element is in the visible area of the browser's viewport.
+ * @param {HTMLElement} elem - The HTML element to check.
+ * @returns {boolean} - Returns true if the element is in the viewport, otherwise returns false.
+ */
 function isInViewport(elem) {
     let distance = elem.getBoundingClientRect();
     return (distance.top < (window.innerHeight || document.documentElement.clientHeight) && distance.bottom > 0);
 }
 
+/**
+ * Manages animations for elements based on their visibility in the viewport.
+ * Checks for specific CSS classes and replaces them with viewport-specific classes if the element is in the viewport.
+ */
 function manageAnimations() {
+    // Animate Slide Left
     let elements = document.getElementsByClassName('animate-slide-left');
     for (let i=0; i<elements.length; i++) {
         if (isInViewport(elements[i])) {
@@ -26,6 +36,7 @@ function manageAnimations() {
             i--;
         }
     }
+    // Animate Slide Right
     elements = document.getElementsByClassName('animate-slide-right');
     for (let i=0; i<elements.length; i++) {
         if (isInViewport(elements[i])) {
@@ -33,6 +44,7 @@ function manageAnimations() {
             i--;
         }
     }
+    // Animate Show Slide
     elements = document.getElementsByClassName('animate-show-slide');
     for (let i=0; i<elements.length; i++) {
         if (isInViewport(elements[i])) {
@@ -42,6 +54,10 @@ function manageAnimations() {
     }
 }
 
+/**
+ * Manages the display of a scroll icon based on the scroll position.
+ * Adds or removes CSS classes to the scroll icon element to control its animation.
+ */
 function showScroll() {
     var scrollIcon = document.getElementById('scroll-icon');
     if (window.scrollY > 0) {
@@ -53,26 +69,36 @@ function showScroll() {
     }
 };
 
+/**
+ * Adjusts the layout of a masonry grid to ensure even heights of cards in each row.
+ * Uses CSS transforms to adjust the vertical position of cards.
+ */
 function adjustMasonry() {
+    // Get the container element and all the card elements inside it
     const container = document.getElementById('card-container');
     const cards = container.querySelectorAll('.card');
 
+    // Reset the vertical position of all cards
     const numCards = cards.length;
     for (let i=0; i<numCards; i++) {
         cards[i].style.transform = 'translateY(0px)';
     }
 
+    // Calculate column width and number of columns and row
     const gap = parseInt(window.getComputedStyle(container).gap);
     const columnWidth = cards[0].offsetWidth + gap;
     const columns = Math.floor(container.offsetWidth / columnWidth) + 1;
     const rows = numCards / columns;
 
+    // Initialize variables
     let diffHeights = [];
     let maxHeights = 0;
     let indexCard = 0;
     let indexAux = 0;
 
+    // If there are more than one column
     if (columns > 1) {
+        // Calculate height differences for each row
         for (let i=0; i<rows; i++) {
             diffHeights[i] = [];
             maxHeights = 0;
@@ -88,6 +114,7 @@ function adjustMasonry() {
                 indexCard++;
             }
         }
+         // If there is more than one row, adjust the vertical position of cards
         if (rows > 1) {
             indexCard = columns;
             let totalDifference = 0;
