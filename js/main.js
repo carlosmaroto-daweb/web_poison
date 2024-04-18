e1c8f54d89bea4c858dbd0b717911d4895cef1c5
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2024-04-18 03:19:20 +0200

    v1.0

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@
 
 window.addEventListener('load', manageAnimations);
 window.addEventListener('load', showScroll);
+window.addEventListener('load', adjustMasonry);
 window.addEventListener('scroll', manageAnimations);
 window.addEventListener('scroll', showScroll);
+window.addEventListener('resize', adjustMasonry);
 
 /*****************************************************************************/
 /** FUNCTIONS **/
@@ -50,3 +52,26 @@ function showScroll() {
         scrollIcon.classList.remove('animate-slide-bottom');
     }
 };
+
+function adjustMasonry() {
+    /*
+    const container = document.getElementById('card-container');
+    const cards = container.querySelectorAll('.card');
+
+    const columns = 4; // Número de columnas
+    const columnHeights = Array.from({ length: columns }, () => 0);
+
+    cards.forEach(card => {
+        const minHeightIndex = columnHeights.indexOf(Math.min(...columnHeights));
+        const minHeight = columnHeights[minHeightIndex];
+
+        card.style.position = 'absolute';
+        card.style.left = `${minHeightIndex * (100 / columns)}%`; // Calcula la posición horizontal basada en el número de columnas
+        card.style.top = `${minHeight}px`; // Asigna la posición vertical basada en la columna más corta
+
+        columnHeights[minHeightIndex] += card.offsetHeight + 30; // Suma la altura de la tarjeta a la columna
+    });
+
+    container.style.height = `${Math.max(...columnHeights)}px`; // Establece la altura del contenedor basada en la columna más alta
+    */
+}
