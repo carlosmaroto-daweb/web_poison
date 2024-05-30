d0461efaa8eed9cac8ee199f18a6966a08d9da2a
Carlos Maroto Delgado <carlosmaroto.daweb@gmail.com>
2024-05-31 01:11:38 +0200

    v1.0

---
+++ wordpress_files/wp-content/themes/poison/assets/js/main.js
@@ -0,0 +1,277 @@
+/*****************************************************************************/
+/** EVENTS **/
+/*****************************************************************************/
+
+window.addEventListener('load', getInfoPosts);
+window.addEventListener('load', manageAnimations);
+window.addEventListener('load', showScroll);
+window.addEventListener('load', adjustMasonry);
+window.addEventListener('scroll', manageAnimations);
+window.addEventListener('scroll', showScroll);
+window.addEventListener('resize', adjustMasonry);
+
+/*****************************************************************************/
+/** VARIABLES **/
+/*****************************************************************************/
+
+var total_posts;
+var posts_per_page;
+var offset;
+
+/*****************************************************************************/
+/** FUNCTIONS **/
+/*****************************************************************************/
+
+/*
+*  Function to get total_posts and posts_per_page
+*/
+function getInfoPosts () {
+
+    // Define the URL for the AJAX request
+    var ajaxurl = 'https://www.carlosmaroto-daweb.com/poison/wp-admin/admin-ajax.php';
+
+    // Make a fetch request to the defined URL
+    fetch(ajaxurl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'action=get_info_posts'
+    })
+    // Process the response as JSON
+    .then(function(response) {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    // Once the JSON data is received, perform actions with it
+    .then(function(data) {
+        total_posts = data.total_posts;
+        posts_per_page = data.posts_per_page;
+        offset = posts_per_page;
+    })
+    .catch(function(error) {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+/**
+ * Checks if an element is in the visible area of the browser's viewport.
+ * @param {HTMLElement} elem - The HTML element to check.
+ * @returns {boolean} - Returns true if the element is in the viewport, otherwise returns false.
+ */
+function isInViewport(elem) {
+    let distance = elem.getBoundingClientRect();
+    return (distance.top < (window.innerHeight || document.documentElement.clientHeight) && distance.bottom > 0);
+}
+
+/**
+ * Manages animations for elements based on their visibility in the viewport.
+ * Checks for specific CSS classes and replaces them with viewport-specific classes if the element is in the viewport.
+ */
+function manageAnimations() {
+    // Animate Slide Left
+    let elements = document.getElementsByClassName('animate-slide-left');
+    for (let i=0; i<elements.length; i++) {
+        if (isInViewport(elements[i])) {
+            elements[i].classList.replace('animate-slide-left', 'animate-slide-left-viewport');
+            i--;
+        }
+    }
+    // Animate Slide Right
+    elements = document.getElementsByClassName('animate-slide-right');
+    for (let i=0; i<elements.length; i++) {
+        if (isInViewport(elements[i])) {
+            elements[i].classList.replace('animate-slide-right', 'animate-slide-right-viewport');
+            i--;
+        }
+    }
+    // Animate Show Slide
+    elements = document.getElementsByClassName('animate-show-slide');
+    for (let i=0; i<elements.length; i++) {
+        if (isInViewport(elements[i])) {
+            elements[i].classList.replace('animate-show-slide', 'animate-show-slide-viewport');
+            i--;
+        }
+    }
+}
+
+/**
+ * Manages the display of a scroll icon based on the scroll position.
+ * Adds or removes CSS classes to the scroll icon element to control its animation.
+ */
+function showScroll() {
+    var scrollIcon = document.getElementById('scroll-icon');
+    if (scrollIcon) {
+        if (window.scrollY > 0) {
+            scrollIcon.classList.add('animate-slide-bottom');
+            scrollIcon.classList.remove('animate-slide-bottom-viewport');
+        } else {
+            scrollIcon.classList.add('animate-slide-bottom-viewport');
+            scrollIcon.classList.remove('animate-slide-bottom');
+        }
+    }
+};
+
+/**
+ * Adjusts the layout of a masonry grid to ensure even heights of cards in each row.
+ * Uses CSS transforms to adjust the vertical position of cards.
+ */
+function adjustMasonry() {
+    // Get the container element
+    const container = document.getElementById('card-container');
+
+    // Check if the container exists
+    if (container) {
+
+        // Get all card elements inside the container and check if there are any card elements
+        const cards = container.querySelectorAll('.card');
+        if (cards.length > 0) {
+
+            // Reset the vertical position of all cards
+            const numCards = cards.length;
+            for (let i=0; i<numCards; i++) {
+                cards[i].style.transform = 'translateY(0px)';
+            }
+
+            // Check if the display style of the element is "grid"
+            let containerDisplay = window.getComputedStyle(container).display;
+            if (containerDisplay == "grid") {
+
+                // Calculate column width and number of columns and row
+                const gap = parseInt(window.getComputedStyle(container).gap);
+                const columnWidth = cards[0].offsetWidth + gap;
+                const columns = Math.floor(container.offsetWidth / columnWidth) + 1;
+                const rows = numCards / columns;
+
+                // Initialize variables for storing height differences
+                let diffHeights = [];
+                let maxHeights = 0;
+                let indexCard = 0;
+                let indexAux = 0;
+
+                // If there are more than one column
+                if (columns > 1) {
+                    // Calculate height differences for each row
+                    for (let i=0; i<rows; i++) {
+                        diffHeights[i] = [];
+                        maxHeights = 0;
+                        indexAux = indexCard;
+                        for (let j=0; j<columns && indexAux<numCards; j++) {
+                            if (cards[indexAux].clientHeight > maxHeights) {
+                                maxHeights = cards[indexAux].clientHeight;
+                            }
+                            indexAux++;
+                        }
+                        for (let j=0; j<columns && indexAux<numCards; j++) {
+                            diffHeights[i][j] = maxHeights - cards[indexCard].clientHeight;
+                            indexCard++;
+                        }
+                    }
+                    // If there is more than one row, adjust the vertical position of cards
+                    if (rows > 1) {
+                        indexCard = columns;
+                        let totalDifference = 0;
+                        for (let i=1; i<rows; i++) {
+                            for (let j=0; j<columns && indexCard<numCards; j++) {
+                                totalDifference = 0;
+                                for (let k=0; k<i; k++) {
+                                    totalDifference += diffHeights[k][j];
+                                }
+                                cards[indexCard].style.transform = 'translateY(' + (- totalDifference) + 'px)';
+                                indexCard++;
+                            }
+                        }
+                    }
+                }
+                // Show 'load-more-posts' button if current offset is less than total posts
+                if (offset < total_posts) {
+                    document.getElementById('load-more-posts').style.display = 'block';
+                }
+            }
+            else {
+                // Enable horizontal scrolling of the container using mouse events
+                let isDown = false;
+                let startX;
+                let scrollLeft;
+                let isLoading = false;
+                
+                container.addEventListener('mousedown', (event) => {
+                    isDown = true;
+                    startX = event.pageX - container.offsetLeft;
+                    scrollLeft = container.scrollLeft;
+                    isLoading = false;
+                });
+                container.addEventListener('mouseleave', () => {
+                    isDown = false;
+                });
+                container.addEventListener('mouseup', () => {
+                    isDown = false;
+                });
+                container.addEventListener('mousemove', (event) => {
+                    if(!isDown) return;
+                    event.preventDefault();
+                    const x = event.pageX - container.offsetLeft;
+                    const walk = x - startX;
+                    container.scrollLeft = scrollLeft - walk;
+                    container.style.scrollBehavior = 'auto';
+
+                    // Check if we've reached the end of the container
+                    if (!isLoading && container.scrollLeft + container.clientWidth >= container.scrollWidth && offset < total_posts) {
+                        loadMorePosts();
+                        isLoading = true;
+                    }
+                });
+                
+                // Hidden 'load-more-posts' button
+                document.getElementById('load-more-posts').style.display = 'none';
+            }
+        }
+    }
+}
+
+
+let button_load_more_posts = document.getElementById('load-more-posts');
+if (button_load_more_posts) {
+    button_load_more_posts.addEventListener('click', loadMorePosts);
+}
+
+function loadMorePosts () {
+
+    // Define the URL for the AJAX request
+    var ajaxurl = 'https://www.carlosmaroto-daweb.com/poison/wp-admin/admin-ajax.php';
+
+    // Make a fetch request to the defined URL
+    fetch(ajaxurl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'action=load_more_posts&offset=' + offset
+    })
+    // Process the response as JSON
+    .then(function(response) {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    // Once the JSON data is received, perform actions with it
+    .then(function(data) {
+        // Insert the received posts data into the HTML element with the id 'card-container'
+        document.getElementById('card-container').insertAdjacentHTML('beforeend', data.posts);
+        // Call functions to manage animations and adjust masonry layout
+        manageAnimations();
+        adjustMasonry();
+        // Increment the offset by posts_per_page
+        offset += posts_per_page;
+        // If the offset exceeds or equals the total number of posts, hide the 'load-more-posts' button
+        if (offset >= total_posts) {
+            document.getElementById('load-more-posts').style.display = 'none';
+        }
+    })
+    .catch(function(error) {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
